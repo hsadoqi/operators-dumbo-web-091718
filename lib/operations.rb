f7804ebcda6645bc36dbe2b60cf7eb58521ecda7
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-27 02:10:10 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,10 +9,5 @@ def unsafe?(speed)
 end 
 
 def not_safe?(speed)
-  if speed > 60
-    return true 
-    elsif speed < 40 
-    return true 
-    
-  end
+  speed > 60 && speed < 40 ? true: false
 end 
